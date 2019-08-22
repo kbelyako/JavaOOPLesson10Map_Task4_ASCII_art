a2f2a9466e36de550da425b02562b1a7b9b72f97
kbelyako <43518418+kbelyako@users.noreply.github.com>
2019-08-22 09:16:09 +0600

    удалено нижнее подчёркивание из имён переменных

--- JavaOOPLesson10Map_Task4_ASCII_art/src/com/yandex/kbelyako/AsciiDict.java
+++ JavaOOPLesson10Map_Task4_ASCII_art/src/com/yandex/kbelyako/AsciiDict.java
@@ -31,26 +31,26 @@ public class AsciiDict {
 		String[] array = new String[6];
 		String res = "";
 		try (BufferedReader f = new BufferedReader(new FileReader(path))) {
-			String temp_str = "";
+			String tempStr = "";
 			int lineCounter = 0;
-			for (; (temp_str = f.readLine()) != null;) {
+			for (; (tempStr = f.readLine()) != null;) {
 				lineCounter = lineCounter + 1;
-				// if (temp_str.charAt(0)!=' ' || temp_str.charAt(0)!='*') {
-				if (temp_str.matches("[A-Z?]")) {
-					char key = temp_str.charAt(0);
-					String[] asii_Char = new String[9];
+				// if (tempStr.charAt(0)!=' ' || tempStr.charAt(0)!='*') {
+				if (tempStr.matches("[A-Z?]")) {
+					char key = tempStr.charAt(0);				
+					String[] asiiChar = new String[9];
 					for (int i = 0; i < 9; i++) {
 						String str2 = "";
-						temp_str = f.readLine();
-						for (int j = 0; j <= 12 - temp_str.length(); j++) {
+						tempStr = f.readLine();
+						for (int j = 0; j <= 12 - tempStr.length(); j++) {
 							str2 = str2 + " ";
 						}
-						temp_str = temp_str + str2;
-						 //System.out.println(temp_str);
-						asii_Char[i] = temp_str;
+						tempStr = tempStr + str2;
+						 //System.out.println(tempStr);
+						asiiChar[i] = tempStr;
 					}
-					// dict.put(key, asii_Char);
-					dict.put(key, asii_Char);
+					// dict.put(key, asiiChar);
+					dict.put(key, asiiChar);
 				}
 			}
 
@@ -63,12 +63,12 @@ public class AsciiDict {
 
 	public void print(String text) {
 		char[] charArray = text.toCharArray();
-		String temp_str = "";
+		String tempStr = "";
 		for (int j = 0; j < this.heigh; j++) {
 			String res_str = "";
 			for (int i = 0; i < charArray.length; i++) {
-				temp_str = dict.get(charArray[i])[j];
-				res_str = res_str + temp_str;
+				tempStr = dict.get(charArray[i])[j];
+				res_str = res_str + tempStr;
 			}
 			System.out.println(res_str);
 		}
@@ -76,11 +76,11 @@ public class AsciiDict {
 	
 	private void addBlank(Map<Character, String[]> inMap) {
 		
-		String[] asii_Char = new String[9];
-		for (int i = 0; i < asii_Char.length; i++) {
-			asii_Char[i]="          ";
+		String[] asiiChar = new String[9];
+		for (int i = 0; i < asiiChar.length; i++) {
+			asiiChar[i]="          ";
 		}		
-		inMap.put(' ', asii_Char);
+		inMap.put(' ', asiiChar);
 		//Map<Character, String[]> dict = new HashMap();
 
 	}
